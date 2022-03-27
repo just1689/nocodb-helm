d1accdaf0b9c88d1f09433e238760db300f0d521
Just1689 <Just1689@gmail.com>
2022-03-27 22:06:32 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nocodb-helm
 Unofficial Helm Chart for nocodb
+
+## Using the chart
+
+```bash
+helm install app captains-charts/nocodb
+```
---
+++ helm-build.sh
@@ -0,0 +1,4 @@
+helm package nocodb
+mv *.tgz ../charts/
+helm repo index --merge ../charts/index.yaml  ../charts/ --url https://storage.googleapis.com/captains-charts/
+# TODO: Copy dir to cloud
